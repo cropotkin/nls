4f217cb369491112c7fb1b9ccba90ac9dc91ae55
Graham Darnell <graham.darnell@linn.co.uk>
2016-12-19 13:03:10 +0000

    Fixed ws281x driver

--- src/LightStripDriver/LightStripDriverWs281x.ts
+++ src/LightStripDriver/LightStripDriverWs281x.ts
@@ -9,7 +9,7 @@ import ILightStripDriver = require("./ILightStripDriver");
 class LightStripDriverWs281x implements ILightStripDriver {
     constructor() {
         this.info = new LightStripInfo("ws281x", 300, 100);
-        ws281x.reset();
+        ws281x.reset(300);
     }
 
     public info : ILightStripInfo;
@@ -23,7 +23,7 @@ class LightStripDriverWs281x implements ILightStripDriver {
     } 
 
     public close() : void {
-        ws281x.reset();
+        ws281x.reset(300);
     }
 }
 
